6a7f41bec12d9147a8b992efa1120693362cfabf
krkshs <kilikrik9@gmail.com>
2026-06-07 06:04:29 +0800

    Refactor debug into files and folder

--- debug/compiler.js
+++ debug/compiler.js
@@ -1,36 +1,6 @@
 const fs = require('fs');
 const crypto = require('crypto');
-
-//core rc4 cipher stream
-function gstream(khex, olen) {
-  const sbox = Array.from({ length: 256 }, (_, i) => i);
-  let mx = 0;
-  for (let i = 0; i < 256; i++) {
-    mx = (mx + sbox[i] + khex.charCodeAt(i % khex.length)) % 256;
-    [sbox[i], sbox[mx]] = [sbox[mx], sbox[i]];
-  }
-  
-  let i = 0, j = 0;
-  const strm = [];
-  for (let n = 0; n < olen; n++) {
-    i = (i + 1) % 256;
-    j = (j + sbox[i]) % 256;
-    [sbox[i], sbox[j]] = [sbox[j], sbox[i]];
-    strm.push(sbox[(sbox[i] + sbox[j]) % 256]);
-  }
-  return strm;
-}
-
-//encrypt payload
-function encdata(rbuf, kstr) {
-  const cbytes = gstream(kstr, rbuf.length);
-  let hdump = '';
-  for (let i = 0; i < rbuf.length; i++) {
-    const xrd = rbuf[i] ^ cbytes[i];
-    hdump += xrd.toString(16).padStart(2, '0');
-  }
-  return hdump;
-}
+const { encdata } = require('./crypto.js');
 
 //process file routine
 function bfridobf(tpath) {
@@ -85,11 +55,4 @@ function bfridobf(tpath) {
   }
 }
 
-//run cli
-const ipath = process.argv[2];
-if (!ipath) {
-  console.error('[-] missing input file');
-  process.exit(1);
-}
-
-bfridobf(ipath);
+module.exports = { bfridobf };
